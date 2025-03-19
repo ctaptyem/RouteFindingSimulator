19a6bd8fa4edd94c5e58c24923cc62cfb5c6d632
Andrei Chekmasov <ctaptyem@gmail.com>
2025-03-19 14:06:36 +0000

    Refactor measurement and plotting code

--- python/collect_measurements.py
+++ python/collect_measurements.py
@@ -11,10 +11,10 @@ def get_command_to_run(output_file, seeds, node_count, edge_percent, pe_grid_siz
                 "-g", f"{int(node_count)},{float(edge_percent)},{int(seeds[0])},{int(seeds[1])}"]
 
 def main():
-    with open("/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime2.csv",'w') as outfile:
+    with open("/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime3.csv",'w') as outfile:
         outfile.write("algorithm,pe_grid_size,node_count,edge_percentage,undirected,weight_mean,weight_std,edge_seed,weight_seed,runtime,commtime,commcount,total_read,total_write\n")
-    random_seeds = [[73, 6135], ]#[8804, 1854], [8224, 2195], ]#[480, 5607], [5764, 4112], [722, 2905], [4776, 3417], [6117, 6371], [9242, 7314], [4399, 4691]]
-    node_counts = np.round(10 * (2 ** np.linspace(0, 5, 7))) #[10,40,160,640]
+    random_seeds = [[73, 6135], [8804, 1854], [8224, 2195], ]#[480, 5607], [5764, 4112], [722, 2905], [4776, 3417], [6117, 6371], [9242, 7314], [4399, 4691]]
+    node_counts = np.round(10 * (2 ** np.linspace(0, 5, 14))) #[10,40,160,640]
     edge_percentages = np.round(np.linspace(0.05,0.95,5),5) # [0.1, 0.3, 0.5, 0.7, 0.9]
     pe_grid_sizes = 2 ** np.linspace(0,4,5) #[1,2,4,8,16]
     algorithms = ['dijkstra', 'foxotto', 'cannons']
@@ -24,7 +24,7 @@ def main():
     print("Starting...")
     subprocess.run(["mvn", "clean", "compile", "assembly:single"], capture_output=True)
     for seeds, node_count, edge_percent, pe_grid_size, algorithm in configs:
-        result = subprocess.run(get_command_to_run("/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime2.csv", seeds,node_count,edge_percent,pe_grid_size,algorithm), capture_output=True)
+        result = subprocess.run(get_command_to_run("/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime3.csv", seeds,node_count,edge_percent,pe_grid_size,algorithm), capture_output=True)
         progress+=1
         print(f"\r{progress}/{total} {seeds, node_count, edge_percent, pe_grid_size, algorithm} {' '*50}",end='')
     print()
--- python/plot_graphs.py
+++ python/plot_graphs.py
@@ -1,174 +1,63 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
 
-def plot_node_count_v_time():
-    df = pd.read_csv('/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime2.csv')
+def plot(df: pd.DataFrame, x_col: str, y_col: str, log_x: bool, log_y: bool, x_name: str = None, y_name: str = None, ylim: float = None):
+    # df['commtime_percent'] = df['commtime'] / df['runtime']
+    if not x_name: x_name = x_col
+    if not y_name: y_name = y_col
 
     fig, ax = plt.subplots(figsize=(12, 8))
-    fig.suptitle("Performance of algorithms for varied node count")
-    ax.set_xlabel("# of nodes in graph (log-scale)")
-    ax.set_ylabel("Execution time (ms) (log-scale)")
+    fig.suptitle(f"{y_name} for varied {x_name}")
+    ax.set_xlabel(f"{x_name} {'(log scale)' if log_x else ''}")
+    ax.set_ylabel(f"{y_name} {'(log scale)' if log_y else ''}")
 
-    grouped_data = df.groupby(['algorithm', 'node_count']).agg({'runtime': ['mean', 'std']}).reset_index()
+    grouped_data = df.groupby(['algorithm', x_col]).agg({y_col: ['mean', 'std']}).reset_index()
     # Flatten the multi-level columns
-    grouped_data.columns = ['algorithm', 'node_count', 'runtime_mean', 'runtime_std']
+    grouped_data.columns = ['algorithm', x_col, f'{y_col}_mean', f'{y_col}_std']
 
     for algo in df['algorithm'].unique():
         algo_data = grouped_data[grouped_data['algorithm'] == algo]
         
         # Plot the mean line
-        ax.plot(algo_data['node_count'], algo_data['runtime_mean'], 
+        ax.plot(algo_data[x_col], algo_data[f'{y_col}_mean'], 
                 marker='o', linewidth=2, label=algo)
         
         # Plot the error range (shaded area)
-        ax.fill_between(algo_data['node_count'], 
-                    algo_data['runtime_mean'] - algo_data['runtime_std'],
-                    algo_data['runtime_mean'] + algo_data['runtime_std'],
+        ax.fill_between(algo_data[x_col], 
+                    algo_data[f'{y_col}_mean'] - algo_data[f'{y_col}_std'],
+                    algo_data[f'{y_col}_mean'] + algo_data[f'{y_col}_std'],
                     alpha=0.1)
 
     ax.grid(True, linestyle='--', alpha=0.7)
     ax.legend(title='Algorithm', fontsize=14, title_fontsize=14)
 
-    ax.set_yscale("log")
-    ax.set_xscale("log")
+    if log_x: ax.set_xscale("log")
+    if log_y: ax.set_yscale("log")
     # Keep x-ticks at node count values
-    ax.set_xticks(df['node_count'].unique())
-    ax.set_xticklabels([str(nc) for nc in df['node_count'].unique()])
+    ax.set_xticks(df[x_col].unique())
+    ax.set_xticklabels([str(nc) for nc in df[x_col].unique()])
     ax.get_xaxis().set_major_formatter(plt.ScalarFormatter())
     ax.get_xaxis().set_tick_params(which='minor', size=0)
     ax.get_xaxis().set_tick_params(which='minor', width=0)
 
-    plt.tight_layout()
-    plt.savefig('/home/andrei/Dev/RouteFindingSimulator/plots/node_count_v_time2.png', dpi=300, bbox_inches='tight')
-
-
-def plot_edge_prop_v_time():
-    df = pd.read_csv('/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime.csv')
-
-    fig, ax = plt.subplots(figsize=(12, 8))
-    fig.suptitle("Performance of algorithms for varied node count")
-    ax.set_xlabel("Percent of possible edges in graph")
-    ax.set_ylabel("Execution time (ms) (log-scale)")
-
-    grouped_data = df.groupby(['algorithm', 'edge_percentage']).agg({'runtime': ['mean', 'std']}).reset_index()
-    # Flatten the multi-level columns
-    grouped_data.columns = ['algorithm', 'edge_percentage', 'runtime_mean', 'runtime_std']
-
-    for algo in df['algorithm'].unique():
-        algo_data = grouped_data[grouped_data['algorithm'] == algo]
-        
-        # Plot the mean line
-        ax.plot(algo_data['edge_percentage'], algo_data['runtime_mean'], 
-                marker='o', linewidth=2, label=algo)
-        
-        # Plot the error range (shaded area)
-        ax.fill_between(algo_data['edge_percentage'], 
-                    algo_data['runtime_mean'] - algo_data['runtime_std'],
-                    algo_data['runtime_mean'] + algo_data['runtime_std'],
-                    alpha=0.1)
-
-    ax.grid(True, linestyle='--', alpha=0.7)
-    ax.legend(title='Algorithm', fontsize=14, title_fontsize=14)
-
-    # ax.set_yscale("log")
-    # ax.set_xscale("log")
-    # Keep x-ticks at node count values
-    ax.set_xticks(df['edge_percentage'].unique())
-    ax.set_xticklabels([str(nc) for nc in df['edge_percentage'].unique()])
-    ax.get_xaxis().set_major_formatter(plt.ScalarFormatter())
-    ax.get_xaxis().set_tick_params(which='minor', size=0)
-    ax.get_xaxis().set_tick_params(which='minor', width=0)
+    if ylim is not None and not log_y:
+        ax.set_ylim(ylim)
 
     plt.tight_layout()
-    plt.savefig('/home/andrei/Dev/RouteFindingSimulator/plots/edge_prop_v_time.png', dpi=300, bbox_inches='tight')
-
-
-def plot_pe_count_v_time():
-    df = pd.read_csv('/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime.csv')
-
-    fig, ax = plt.subplots(figsize=(12, 8))
-    fig.suptitle("Performance of algorithms for varied node count")
-    ax.set_xlabel("Processing Element Grid Size")
-    ax.set_ylabel("Execution time (ms) (log-scale)")
-
-    grouped_data = df.groupby(['algorithm', 'pe_grid_size']).agg({'runtime': ['mean', 'std']}).reset_index()
-    # Flatten the multi-level columns
-    grouped_data.columns = ['algorithm', 'pe_grid_size', 'runtime_mean', 'runtime_std']
-
-    for algo in df['algorithm'].unique():
-        algo_data = grouped_data[grouped_data['algorithm'] == algo]
-        
-        # Plot the mean line
-        ax.plot(algo_data['pe_grid_size'], algo_data['runtime_mean'], 
-                marker='o', linewidth=2, label=algo)
-        
-        # Plot the error range (shaded area)
-        ax.fill_between(algo_data['pe_grid_size'], 
-                    algo_data['runtime_mean'] - algo_data['runtime_std'],
-                    algo_data['runtime_mean'] + algo_data['runtime_std'],
-                    alpha=0.1)
-
-    ax.grid(True, linestyle='--', alpha=0.7)
-    ax.legend(title='Algorithm', fontsize=14, title_fontsize=14)
-
-    ax.set_yscale("log")
-    ax.set_xscale("log")
-    # Keep x-ticks at node count values
-    ax.set_xticks(df['pe_grid_size'].unique())
-    ax.set_xticklabels([str(nc) for nc in df['pe_grid_size'].unique()])
-    ax.get_xaxis().set_major_formatter(plt.ScalarFormatter())
-    ax.get_xaxis().set_tick_params(which='minor', size=0)
-    ax.get_xaxis().set_tick_params(which='minor', width=0)
-
-    plt.tight_layout()
-    plt.savefig('/home/andrei/Dev/RouteFindingSimulator/plots/pe_count_v_time.png', dpi=300, bbox_inches='tight')
-
-def plot_node_count_v_commtime():
-    df = pd.read_csv('/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime2.csv')
-    df.loc[df['algorithm'] == 'DijkstraMCU', 'commtime'] = 0
-    df['commtime_percent'] = df['commtime'] / df['runtime']
-
-    fig, ax = plt.subplots(figsize=(12, 8))
-    fig.suptitle("Performance of algorithms for varied node count")
-    ax.set_xlabel("# of nodes in graph (log-scale)")
-    ax.set_ylabel("Percentage of time communicating (ms) (log-scale)")
-
-    grouped_data = df.groupby(['algorithm', 'node_count']).agg({'commtime_percent': ['mean', 'std']}).reset_index()
-    # Flatten the multi-level columns
-    grouped_data.columns = ['algorithm', 'node_count', 'commtime_mean', 'commtime_std']
-
-    for algo in df['algorithm'].unique():
-        algo_data = grouped_data[grouped_data['algorithm'] == algo]
-        
-        # Plot the mean line
-        ax.plot(algo_data['node_count'], algo_data['commtime_mean'], 
-                marker='o', linewidth=2, label=algo)
-        
-        # Plot the error range (shaded area)
-        ax.fill_between(algo_data['node_count'], 
-                    algo_data['commtime_mean'] - algo_data['commtime_std'],
-                    algo_data['commtime_mean'] + algo_data['commtime_std'],
-                    alpha=0.1)
-
-    ax.grid(True, linestyle='--', alpha=0.7)
-    ax.legend(title='Algorithm', fontsize=14, title_fontsize=14)
-
-    # ax.set_yscale("log")
-    ax.set_xscale("log")
-    # Keep x-ticks at node count values
-    ax.set_xticks(df['node_count'].unique())
-    ax.set_xticklabels([str(nc) for nc in df['node_count'].unique()])
-    ax.get_xaxis().set_major_formatter(plt.ScalarFormatter())
-    ax.get_xaxis().set_tick_params(which='minor', size=0)
-    ax.get_xaxis().set_tick_params(which='minor', width=0)
-
-    plt.tight_layout()
-    plt.savefig('/home/andrei/Dev/RouteFindingSimulator/plots/node_count_v_commtime2.png', dpi=300, bbox_inches='tight')
-
+    plt.savefig(f'/home/andrei/Dev/RouteFindingSimulator/plots/{y_col}_vs_{x_col}.png', dpi=300, bbox_inches='tight')
 
 
 def main():
-    plot_node_count_v_commtime()
+    # algorithm,pe_grid_size,node_count,edge_percentage,undirected,weight_mean,weight_std,edge_seed,weight_seed,runtime,commtime,commcount,total_read,total_write
+    df = pd.read_csv('/home/andrei/Dev/RouteFindingSimulator/measurements/test_commtime3.csv')
+    df['proc_count'] = np.square(df['pe_grid_size'])+1
+    df['commtime_percent'] = df['commtime'] / (df['runtime']+df['commtime'])
+
+    plot(df, 'node_count', 'runtime', True, True, "Node Count", "Execution Time (ms)", ylim=0.0)
+    plot(df, 'edge_percentage', 'runtime', False, False, "Proportion of Edges", "Execution Time (ms)", ylim=0.0)
+    plot(df, 'proc_count', 'runtime', True, False, "Processor Count", "Execution Time (ms)", ylim=0.0)
+    plot(df, 'node_count', 'commtime_percent', True, False, "Node Count", "Communication Time (ms)", ylim=0.0)
 
 if __name__ == "__main__":
     main()
